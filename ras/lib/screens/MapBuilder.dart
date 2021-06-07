7d7b6ddd7e0eae5a5b4ccbf34b32a99994ec5d43
Karine <karine.pistili@gmail.com>
2021-06-07 20:59:03 -0300

    map: get user device location

--- ras/lib/screens/MapBuilder.dart
+++ ras/lib/screens/MapBuilder.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:geolocator/geolocator.dart';
 
 class MapBuilder extends StatefulWidget {
   const MapBuilder({Key? key}) : super(key: key);
@@ -14,16 +15,53 @@ class MapBuilder extends StatefulWidget {
 class _MapBuilderState extends State<MapBuilder> {
   Completer<GoogleMapController> _controller = Completer();
 
-  static final CameraPosition _initPosition = CameraPosition(
+  static CameraPosition _initPosition = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
+    zoom: 15,
   );
 
   _setUserLocation() async {
     if (await Permission.location.request().isGranted) {
       // Either the permission was already granted before or the user just granted it.
-      print('Get the location');
+      _determinePosition();
+    } else {
+      showAlertDialog(
+          'Ops!', 'You need to device location to use this feature');
     }
+  }
+
+  _determinePosition() async {
+    Position position = await GeolocatorPlatform.instance
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    CameraPosition cameraPosition = CameraPosition(
+      target: LatLng(position.latitude, position.longitude),
+      zoom: 15,
+    );
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+  }
+
+  showAlertDialog(String title, String msg) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('$title'),
+            content: Text('$msg'),
+            actions: <Widget>[
+              OutlinedButton(
+                child: Text("CLOSE"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                style: OutlinedButton.styleFrom(
+                  primary: Colors.blue,
+                  side: BorderSide(color: Colors.blue, width: 1),
+                ),
+              ),
+            ],
+          );
+        });
   }
 
   @override
