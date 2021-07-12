62cedf3cbf162a628cba22b19bdf057fe92ee880
Karine <karine.pistili@gmail.com>
2021-07-12 20:07:39 -0300

    implemented splash screen with all logos

--- ras/lib/main.dart
+++ ras/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:ras/screens/AboutScreen.dart';
 import 'package:ras/screens/HomeScreen.dart';
 import 'package:ras/screens/ProjectBuilder.dart';
@@ -6,6 +7,7 @@ import 'package:ras/screens/ProjectView.dart';
 import 'package:ras/screens/SeedForm.dart';
 import 'package:ras/screens/Settings.dart';
 import 'package:ras/screens/MapBuilder.dart';
+import 'package:ras/screens/SplashScreen.dart';
 import 'package:ras/screens/TestPlayground.dart';
 
 void main() {
@@ -16,15 +18,18 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
     return MaterialApp(
       title: 'RAS',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
-      initialRoute: '/',
+      initialRoute: '/splash',
       routes: {
         '/': (context) => MyHomePage(),
+        '/splash': (context) => SplashScreen(),
         '/map': (context) => MapBuilder(),
         '/settings': (context) => Settings(),
         '/project-builder': (context) => ProjectBuilder(),
---
+++ ras/lib/screens/SplashScreen.dart
@@ -0,0 +1,183 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+// List of images
+final List<String> images = [
+  'assets/logoRas.png',
+  'assets/logoLg.png',
+  'assets/logoGsoc.png',
+  'assets/logoWtm.png',
+  'assets/logoLgLab.png',
+  'assets/logoLgEu.png',
+  'assets/logoTic.png', 
+  'assets/logoPcital.jpg',
+  'assets/logoFacens.png', 
+  'assets/logoDronecoria.png', 
+];
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    startTimer();
+  }
+
+  startTimer() async {
+    Duration duration = Duration(seconds: 5);
+
+    return Timer(duration, pushToHome);
+  }
+
+  pushToHome() {
+    Navigator.pushNamed(context, '/');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
+
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                child: Center(
+                  child: Image.asset(
+                    images[0],
+                    fit: BoxFit.contain,
+                  ),
+                ),
+                width: screenSize.width / 5,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal:20.0),
+                child: Text('Reforestation Assistant', textAlign: TextAlign.center, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30, color: Colors.grey.shade700),),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                        horizontal: screenSize.width * 0.05),
+                    child: Container(
+                      child: Center(
+                        child: Image.asset(
+                          images[1],
+                          fit: BoxFit.contain,
+                        ),
+                      ),
+                      width: screenSize.width / 5,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                        horizontal: screenSize.width * 0.05),
+                    child: Container(
+                      child: Center(
+                        child: Image.asset(
+                          images[2],
+                          fit: BoxFit.contain,
+                        ),
+                      ),
+                      width: screenSize.width / 5,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                        horizontal: screenSize.width * 0.05),
+                    child: Container(
+                      child: Center(
+                        child: Image.asset(
+                          images[3],
+                          fit: BoxFit.contain,
+                        ),
+                      ),
+                      width: screenSize.width / 5,
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                    child: Center(
+                      child: Image.asset(
+                        images[4],
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                    width: screenSize.width / 5,
+                  ),
+                  Container(
+                    child: Center(
+                      child: Image.asset(
+                        images[5],
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                    width: screenSize.width / 5,
+                  ),
+                  Container(
+                    child: Center(
+                      child: Image.asset(
+                        images[6],
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                    width: screenSize.width / 5,
+                  ),
+                  Container(
+                    child: Center(
+                      child: Image.asset(
+                        images[7],
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                    width: screenSize.width / 5,
+                  ),
+                ],
+              ),
+               Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                    child: Center(
+                      child: Image.asset(
+                        images[8],
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                    width: screenSize.width / 5,
+                  ),
+                  Container(
+                    child: Center(
+                      child: Image.asset(
+                        images[9],
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                    width: screenSize.width / 5,
+                  ),
+                ],
+              ),
+              CircularProgressIndicator()
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
