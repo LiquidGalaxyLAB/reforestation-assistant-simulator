3c4140a451deba206e526671f9f513c47edc91cf
Karine <karine.pistili@gmail.com>
2021-08-02 21:17:05 -0300

    fixed project view survival info chart

--- ras/lib/widgets/SurvivalInfoChart.dart
+++ ras/lib/widgets/SurvivalInfoChart.dart
@@ -36,13 +36,35 @@ class _SurvivalInfoChartState extends State<SurvivalInfoChart> {
 
   List<ChartData> getChartData() {
     final List<ChartData> chartData = [
-      ChartData('Germinative potential', (0.9 * getTotalItems())),
-      ChartData('Hydric stress', (0.2 * getTotalItems())),
-      ChartData('Thermal stress', (0.2 * getTotalItems())),
-      ChartData('Bad location', (0.5 * getTotalItems())),
-      ChartData('Predation', (0.5 * getTotalItems())),
-      ChartData('Establishment', (0.2 * getTotalItems())),
-      ChartData('Survival', (0.11 * getTotalItems())),
+      ChartData(
+        'Germinative potential',
+        (0.9 * getTotalItems()),
+      ),
+      ChartData(
+        'Hydric stress',
+        (0.2 * (0.9 * getTotalItems())),
+      ),
+      ChartData(
+        'Thermal stress',
+        (0.2 * (0.2 * (0.9 * getTotalItems()))),
+      ),
+      ChartData(
+        'Bad location',
+        (0.5 * (0.2 * (0.2 * (0.9 * getTotalItems())))),
+      ),
+      ChartData(
+        'Predation',
+        (0.5 * (0.5 * (0.2 * (0.2 * (0.9 * getTotalItems()))))),
+      ),
+      ChartData(
+        'Establishment',
+        (0.2 * (0.5 * (0.5 * (0.2 * (0.2 * (0.9 * getTotalItems())))))),
+      ),
+      ChartData(
+        'Survival',
+        (0.11 *
+            (0.2 * (0.5 * (0.5 * (0.2 * (0.2 * (0.9 * getTotalItems()))))))),
+      ),
     ];
 
     return chartData;
@@ -51,12 +73,20 @@ class _SurvivalInfoChartState extends State<SurvivalInfoChart> {
   List<LineChartData> getLineChartData() {
     final List<LineChartData> lineChartData = [
       LineChartData((0.9 * getTotalItems()), 'Germinative potential'),
-      LineChartData((0.2 * getTotalItems()), 'Hydric stress'),
-      LineChartData((0.2 * getTotalItems()), 'Thermal stress'),
-      LineChartData((0.5 * getTotalItems()), 'Bad location'),
-      LineChartData((0.5 * getTotalItems()), 'Predation'),
-      LineChartData((0.2 * getTotalItems()), 'Establishment'),
-      LineChartData((0.11 * getTotalItems()), 'Survival'),
+      LineChartData((0.2 * (0.9 * getTotalItems())), 'Hydric stress'),
+      LineChartData((0.2 * (0.2 * (0.9 * getTotalItems()))), 'Thermal stress'),
+      LineChartData(
+          (0.5 * (0.2 * (0.2 * (0.9 * getTotalItems())))), 'Bad location'),
+      LineChartData(
+          (0.5 * (0.5 * (0.2 * (0.2 * (0.9 * getTotalItems()))))), 'Predation'),
+      LineChartData(
+          (0.2 * (0.5 * (0.5 * (0.2 * (0.2 * (0.9 * getTotalItems())))))),
+          'Establishment'),
+      LineChartData(
+          (0.11 *
+              0.2 *
+              (0.5 * (0.5 * (0.2 * (0.2 * (0.9 * getTotalItems())))))),
+          'Survival'),
     ];
 
     return lineChartData;
@@ -68,6 +98,7 @@ class _SurvivalInfoChartState extends State<SurvivalInfoChart> {
       child: SfCartesianChart(
         series: <ChartSeries>[
           ColumnSeries<ChartData, String>(
+              dataLabelSettings: DataLabelSettings(isVisible: true, labelAlignment: ChartDataLabelAlignment.top),
               dataSource: _chartData,
               xValueMapper: (ChartData e, _) => e.label,
               yValueMapper: (ChartData e, _) => e.value,
