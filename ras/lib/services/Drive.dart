aa707f041f2c9cd290eeaa5df0426b07c360d0ac
Karine <karine.pistili@gmail.com>
2021-08-13 17:33:37 -0300

    implemented upload kml to drive

--- ras/lib/services/Drive.dart
+++ ras/lib/services/Drive.dart
@@ -4,6 +4,8 @@ import 'package:googleapis/drive/v3.dart' as drive;
 import 'package:google_sign_in/google_sign_in.dart' as signIn;
 import 'package:http/http.dart' as http;
 import 'package:ras/models/Project.dart';
+import 'package:ras/models/kml/Kml.dart';
+import 'package:ras/services/KmlGenerator.dart';
 import 'package:ras/services/PdfGenerator.dart';
 
 class GoogleDrive {
@@ -12,8 +14,10 @@ class GoogleDrive {
         signIn.GoogleSignIn.standard(scopes: [drive.DriveApi.driveScope]);
     final signIn.GoogleSignInAccount? account = await googleSignIn.signIn();
     print("User account $account");
-    if (account != null) uploadFile(account, project);
-    else return Future.error('Error uploading to drive');
+    if (account != null)
+      uploadFile(account, project);
+    else
+      return Future.error('Error uploading to drive');
   }
 
   uploadFile(signIn.GoogleSignInAccount account, Project pr) async {
@@ -21,14 +25,39 @@ class GoogleDrive {
     final authenticateClient = GoogleAuthClient(authHeaders);
     final driveApi = drive.DriveApi(authenticateClient);
 
+    await uploadKMLFile(pr, driveApi);
+
+    // Upload PDF
     File pdf = await PdfGenerator.generatePdf(pr);
     var driveFile = new drive.File();
-    driveFile.name = "${pr.projectName}";
+    driveFile.name = "${pr.projectName}.pdf";
 
     try {
       final result = await driveApi.files.create(driveFile,
           uploadMedia: drive.Media(pdf.openRead(), pdf.lengthSync()));
       await pdf.delete();
+      return Future.value(result);
+    } catch (e) {
+      return Future.error('Error uploading to drive $e');
+    }
+  }
+
+  uploadKMLFile(Project pr, driveApi) async {
+    // Upload KML
+    // create kml based on geodata attribute
+    String content = KML.buildKMLContent(
+        pr.geodata.markers, pr.geodata.areaPolygon, pr.geodata.landingPoint);
+    KML kml = KML(pr.projectName, content);
+    final kmlDone = kml.mount();
+    File kmlFile = await KMLGenerator.generateKML(kmlDone, pr.projectName);
+    
+    var driveFile = new drive.File();
+    driveFile.name = "${pr.projectName}.kml";
+
+    try {
+      final result = await driveApi.files.create(driveFile,
+          uploadMedia: drive.Media(kmlFile.openRead(), kmlFile.lengthSync()));
+      await kmlFile.delete();
       return Future.value(result);
     } catch (e) {
       return Future.error('Error uploading to drive $e');
