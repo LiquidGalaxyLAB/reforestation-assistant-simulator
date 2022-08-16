c3ddf0cb3e304cccaa264430d0ae20c466750f53
Syed Ali Ahmed <sahmed63@hawk.iit.edu>
2022-08-16 13:27:11 -0700

    Updated About Screen

--- ras/lib/screens/AboutScreen.dart
+++ ras/lib/screens/AboutScreen.dart
@@ -19,7 +19,7 @@ class _AboutScreenState extends State<AboutScreen> {
     'assets/logos/logoLgEu.png',
     'assets/logos/logoTic.png',
     'assets/logos/logoPcital.jpg',
-    'assets/logos/logoFacens.png',
+    'assets/logos/logoAera.jpeg',
     'assets/logos/logoDronecoria.png',
   ];
 
@@ -86,23 +86,171 @@ class _AboutScreenState extends State<AboutScreen> {
                     child: Text('View Privacy Policy'),
                   ),
                 ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 15.0),
-                child: Text(
-                  'This project was created during the Google Summer of Code 2021 alongside the Liquid Galaxy and Dronecoria organizations. \n\nThis open source project consists of an app to help plan and monitor reforestation projects either by sowning drones or manually. \n\nUsers can plan the reforesting missions, by defining areas of seeding, specific seed/tree location, drone landing points, fly zones and other helpful metrics. All the gathered information can be dynamically displayed on the Liquid Galaxy cluster using KMLs and also on the app with the help of charts and text to bring users a great and immersive overview of each projects\' panorama.\n',
-                  style: TextStyle(fontSize: 17),
-                  textAlign: TextAlign.center,
-                ),
-              ),
               Text(
                 'Contributors',
                 textAlign: TextAlign.center,
                 style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
               ),
               Text(
-                '\n Syed Ali Ahmed (GSoC 2022)\n\n Karine Pistili (GSoC 2021)\n',
+                '\n Syed Ali Ahmed (GSoC 2022)',
                 textAlign: TextAlign.center,
                 style: TextStyle(fontSize: 17),
+              ),
+              Text(
+                '\n Karine Pistili (GSoC 2021)\n',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 17),
+              ),
+              Text(
+                'Mentors',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+              ),
+              Text(
+                '\n Lot Amorós & Karine Pistili (GSoC 2022)\n\n Lot Amorós (GSoC 2021)\n',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 17),
+              ),
+              Text(
+                'Contact Us',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+              ),
+              GestureDetector(
+                onTap: () {
+                  _launchURL('https://www.github.com/whysyed');
+                },
+                child: Text(
+                  '\nSyed\'s Github',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 17, color: Colors.blue),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  _launchURL('https://github.com/KarinePistili');
+                },
+                child: Text(
+                  '\nKarine\'s Github',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 17, color: Colors.blue),
+                ),
+              ),
+              Text(
+                '\nOur partners',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: screenSize.width * 0.05, vertical: 30),
+                        child: Container(
+                          child: Center(
+                            child: Image.asset(
+                              images[1],
+                              fit: BoxFit.contain,
+                            ),
+                          ),
+                          width: screenSize.width / 6,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Container(
+                          child: Center(
+                            child: Image.asset(
+                              images[2],
+                              fit: BoxFit.contain,
+                            ),
+                          ),
+                          width: screenSize.width / 6,
+                        ),
+                      Container(
+                        child: Center(
+                          child: Image.asset(
+                            images[4],
+                            fit: BoxFit.contain,
+                          ),
+                        ),
+                        width: screenSize.width / 6,
+                      ),
+                      Container(
+                        child: Center(
+                          child: Image.asset(
+                            images[5],
+                            fit: BoxFit.contain,
+                          ),
+                        ),
+                        width: screenSize.width / 6,
+                      ),
+                      Container(
+                        child: Center(
+                          child: Image.asset(
+                            images[6],
+                            fit: BoxFit.contain,
+                          ),
+                        ),
+                        width: screenSize.width / 6,
+                      ),
+                      Container(
+                        child: Center(
+                          child: Image.asset(
+                            images[7],
+                            fit: BoxFit.contain,
+                          ),
+                        ),
+                        width: screenSize.width / 6,
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 30.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Container(
+                          child: Center(
+                            child: Image.asset(
+                              images[8],
+                              fit: BoxFit.contain,
+                            ),
+                          ),
+                          width: screenSize.width / 4,
+                        ),
+                        Container(
+                          child: Center(
+                            child: Image.asset(
+                              images[9],
+                              fit: BoxFit.contain,
+                            ),
+                          ),
+                          width: screenSize.width / 3,
+                        ),
+                      ],
+                    ),
+                  ),
+              Text(
+                'Description',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+              ),
+                Padding(
+                padding: const EdgeInsets.symmetric(vertical: 15.0),
+                child: Text(
+                  'This project was created during the Google Summer of Code 2021 alongside the Liquid Galaxy and Dronecoria organizations. \n\nThis open source project consists of an app to help plan and monitor reforestation projects either by sowning drones or manually. \n\nUsers can plan the reforesting missions, by defining areas of seeding, specific seed/tree location, drone landing points, fly zones and other helpful metrics. All the gathered information can be dynamically displayed on the Liquid Galaxy cluster using KMLs and also on the app with the help of charts and text to bring users a great and immersive overview of each projects\' panorama.\n',
+                  style: TextStyle(fontSize: 17),
+                  textAlign: TextAlign.center,
+                ),
               ),
               Text(
                 'Learn more',
@@ -172,126 +320,6 @@ class _AboutScreenState extends State<AboutScreen> {
                   ),
                 ),
               ),
-              Text(
-                '\nOur partners',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.symmetric(
-                            horizontal: screenSize.width * 0.05, vertical: 30),
-                        child: Container(
-                          child: Center(
-                            child: Image.asset(
-                              images[1],
-                              fit: BoxFit.contain,
-                            ),
-                          ),
-                          width: screenSize.width / 6,
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.symmetric(
-                            horizontal: screenSize.width * 0.05),
-                        child: Container(
-                          child: Center(
-                            child: Image.asset(
-                              images[2],
-                              fit: BoxFit.contain,
-                            ),
-                          ),
-                          width: screenSize.width / 5,
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.symmetric(
-                            horizontal: screenSize.width * 0.05),
-                        child: Container(
-                          child: Center(
-                            child: Image.asset(
-                              images[3],
-                              fit: BoxFit.contain,
-                            ),
-                          ),
-                          width: screenSize.width / 5,
-                        ),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Container(
-                        child: Center(
-                          child: Image.asset(
-                            images[4],
-                            fit: BoxFit.contain,
-                          ),
-                        ),
-                        width: screenSize.width / 5,
-                      ),
-                      Container(
-                        child: Center(
-                          child: Image.asset(
-                            images[5],
-                            fit: BoxFit.contain,
-                          ),
-                        ),
-                        width: screenSize.width / 5,
-                      ),
-                      Container(
-                        child: Center(
-                          child: Image.asset(
-                            images[6],
-                            fit: BoxFit.contain,
-                          ),
-                        ),
-                        width: screenSize.width / 5,
-                      ),
-                      Container(
-                        child: Center(
-                          child: Image.asset(
-                            images[7],
-                            fit: BoxFit.contain,
-                          ),
-                        ),
-                        width: screenSize.width / 5,
-                      ),
-                    ],
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 30.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Container(
-                          child: Center(
-                            child: Image.asset(
-                              images[8],
-                              fit: BoxFit.contain,
-                            ),
-                          ),
-                          width: screenSize.width / 4,
-                        ),
-                        Container(
-                          child: Center(
-                            child: Image.asset(
-                              images[9],
-                              fit: BoxFit.contain,
-                            ),
-                          ),
-                          width: screenSize.width / 3,
-                        ),
-                      ],
-                    ),
-                  ),
                 ],
               ),
             ],
