1ad11d4fc6fb8a7d855df855e898d3bc3f94e4c9
Syed Ali Ahmed <sahmed63@hawk.iit.edu>
2022-09-06 23:41:54 -0500

    new pdf format

--- ras/lib/services/PdfGenerator.dart
+++ ras/lib/services/PdfGenerator.dart
@@ -112,7 +112,27 @@ class PdfGenerator {
     final downloadsDirectory = await DownloadsPathProvider.downloadsDirectory;
     final tempDirectory = await getTemporaryDirectory();
     var savePath = tempDirectory.path;
-    final image = pw.MemoryImage(await File("$savePath/graphs.png").readAsBytes());
+    final image1 = pw.MemoryImage(await File("$savePath/graphs1.png").readAsBytes());
+    final image2 = pw.MemoryImage(await File("$savePath/graphs2.png").readAsBytes());
+    final image3 = pw.MemoryImage(await File("$savePath/graphs3.png").readAsBytes());
+
+    var seedsAsMap = <Map<String, String>>[
+    for(int i = 0; i < project.seeds.length; i++)
+      {
+        "common_name": "${project.seeds[i].commonName}",
+        "density": "${project.seeds[i].density}",
+        "co2PerYear": "${project.seeds[i].co2PerYear}",
+        "estimatedLongevity": "${project.seeds[i].estimatedLongevity}",
+        "estimatedFinalHeight": "${project.seeds[i].estimatedFinalHeight}",
+        "seedballDiameter": "${project.seeds[i].seedballDiameter}",
+      },
+  ];
+
+  List<List<String>> listSeeds=List.empty(growable: true);
+  for(int i=0;i<seedsAsMap.length;i++)
+  {
+    listSeeds.add(seedsAsMap[i].values.toList());
+  }
 
     pdf.addPage(
       pw.MultiPage(
@@ -125,82 +145,109 @@ class PdfGenerator {
                   pw.TextStyle(fontWeight: pw.FontWeight.bold, fontSize: 30),
             ),
             title('BASIC INFORMATION'),
-            attribute('Date of project',
-                '${project.dateOfProject.toString().substring(0, 10)}'),
-            attribute('Area covered', 'XX'),
-            attribute('Region', '${project.region}'),
-            attribute('Sown mode', '${project.sownMode}'),
+            pw.Table.fromTextArray(context: context, data: <List<String>>[
+              <String>['Date of project', 'Region', 'Sown mode'],
+              <String>['${project.dateOfProject.toString().substring(0, 10)}', 
+              '${project.region}',
+              '${project.sownMode}'
+              ],
+            ]),
             title('PROJECT INFORMATION'),
-            attribute('Total Flights', '${getTotalFlights()}'),
-            attribute('CO2 capture until today', '${getCO2()}' + ' kg'),
-            attribute('Planned CO2 capture', '${getCO2Planned()}' + ' kg'),
-            attribute('Size of Deposit', '${project.sizeOfDeposit}% liters'),
-            attribute('Time of Flight', '${project.timeOfFlight}' + 'min/hectare'),
+            pw.Table.fromTextArray(context: context, data: <List<String>>[
+              <String>['Total Flights', 'CO2 capture until today', 'Planned CO2 capture', 'Size of Deposit', 'Time of Flight'],
+              <String>['${getTotalFlights()}',
+              '${getCO2()}' + ' kg',
+              '${getCO2Planned()}' + ' kg',
+              '${project.sizeOfDeposit}% liters',
+              '${project.timeOfFlight}' + 'min/hectare'
+              ],
+            ]),
             title('SOWING WINDOW TIME'),
-            subtitle('DATES'),
-            attribute(
-                'Minimum', '${project.minSwtDate.toString().substring(0, 10)}'),
-            attribute(
-                'Maximum', '${project.maxSwtDate.toString().substring(0, 10)}'),
+            pw.Table.fromTextArray(context: context, data: <List<String>>[
+              <String>['Minimum Date', 'Maximum Date'],
+              <String>['${project.minSwtDate.toString().substring(0, 10)}',
+              '${project.maxSwtDate.toString().substring(0, 10)}'
+              ],
+            ]),
             subtitle('TEMPERATURE'),
-            attribute('Minimum', '${project.minSwtTemp}°C'),
-            attribute('Maximum', '${project.maxSwtTemp}°C'),
-            attribute('Average number of rains', '${project.avgNumberOfRains}'),
-            attribute(
-                'Total number of rainy days', '${project.totalNumberOfRains}'),
+            pw.Table.fromTextArray(context: context, data: <List<String>>[
+              <String>['Minimum Temp', 'Maximum Temp', 'Avg number of rains', 'Total rainy days'],
+              <String>['${project.minSwtTemp}°C',
+              '${project.maxSwtTemp}°C', '${project.avgNumberOfRains}', '${project.totalNumberOfRains}'
+              ],
+            ]),
             title('SPECIES INFORMATION'),
-            subtitle('SEEDS'),
-            pw.ListView.builder(
-                itemBuilder: (pw.Context context, int index) {
-                  return pw.Padding(
-                    padding: pw.EdgeInsets.symmetric(vertical: 10),
-                    child: pw.Column(children: [
-                      attribute(
-                          'Common name', '${project.seeds[index].commonName}'),
-                      attribute(
-                          'Density', '${project.seeds[index].density} plants per hectare'),
-                      attribute('Survival probability', 'XX'),
-                      attribute('Estimated CO2 capture',
-                          '${project.seeds[index].co2PerYear} kg per year'),
-                      attribute('Estimated longevity',
-                          '${project.seeds[index].estimatedLongevity} years'),
-                      attribute('Estimated final height',
-                          '${project.seeds[index].estimatedFinalHeight} m'),
-                      attribute(
-                          'Seedball Diameter', '${project.seeds[index].seedballDiameter} mm'),
-                    ]),
-                  );
-                },
-                itemCount: project.seeds.length),
+            pw.Table.fromTextArray(context: context, headers: <String>['Common Name', 'Density', 'CO2 capture', 
+              'Estimated longevity', 'Estimated final height', 'Seedball Diameter'], data: listSeeds
+            ),
             title('AREA INFORMATION'),
-            attribute('Area covered', '${getArea(project).toStringAsFixed(2)} hectares'),
-            attribute('Optimal surface', '${project.validSurface}%'),
-            attribute('Invalid surface', '${project.notValidSurface}%'),
-            attribute('Empty land', '${project.emptyLand}%'),
-            attribute('Orientation', '${project.orientation}'),
-            attribute('Minimum altitude of the terrain',
-                '${project.minAltTerrain} m'),
-            attribute('Maximum altitude of the terrain',
-                '${project.maxAltTerrain} m'),
-            attribute('Maximum distance', '${project.maxDistance} m'),
-            attribute('Minimum flight height', '${project.minFlightHeight} m'),
-            attribute('Predation', '${project.predation}%'),
+            pw.Table.fromTextArray(context: context, data: <List<String>>[
+              <String>['Area covered', 'Optimal surface', 'Invalid surface', 'Empty land', 'Orientation', 'Min altitude', 'Max altitude', 'Max distance', 'Min flight height', 'Predation'],
+              <String>['${getArea(project).toStringAsFixed(2)} hectares', 
+              '${project.validSurface}%',
+              '${project.notValidSurface}%',
+              '${project.emptyLand}%',
+              '${project.orientation}','${project.minAltTerrain} m','${project.maxAltTerrain} m',
+              '${project.maxDistance} m','${project.minFlightHeight} m', '${project.predation}%'
+              ],
+            ]),
             title('SOIL ATTRIBUTES'),
-            attribute('Depth', '${project.depth} m'),
-            attribute('PH', '${project.ph}'),
-            attribute('Fractured', '${project.fractured ? 'Yes' : 'No'}'),
-            attribute('Hummus presence', '${project.hummus}'),
-            attribute('Inclination',
-                '${project.inclination}% | ${(project.inclination / 100) * 360}°'),
+            pw.Table.fromTextArray(context: context, data: <List<String>>[
+              <String>['Depth', 'PH', 'Fractured', 'Hummus presence', 'Inclination'],
+              <String>['${project.depth} m',
+              '${project.ph}',
+              '${project.fractured ? 'Yes' : 'No'}',
+              '${project.hummus}',
+              '${project.inclination}% | ${(project.inclination / 100) * 360}°'
+              ],
+            ]),
           ];
         },
       ),
     );
 
-    pdf.addPage(pw.Page(build: (pw.Context context) {
-      return pw.Center(
-        child: pw.Image(image),
-      );
+    pdf.addPage(pw.MultiPage(build: (pw.Context context) {
+      return [ 
+        pw.Row(
+            children: [
+            pw.Container(
+              width: 250,
+              height: 720,
+              decoration: pw.BoxDecoration(
+                image: pw.DecorationImage(
+                  fit: pw.BoxFit.contain,
+                  image: image1
+                ),
+              ),
+            ),
+            pw.Spacer(flex: 1),
+            pw.Column(
+              children: [
+                          pw.Container(
+  width: 200,
+  height: 320,
+  decoration: pw.BoxDecoration(
+    image: pw.DecorationImage(
+      fit: pw.BoxFit.contain,
+      image: image2
+    ),
+  ),
+),
+            pw.Container(
+  width: 200,
+  height: 320,
+  decoration: pw.BoxDecoration(
+    image: pw.DecorationImage(
+      fit: pw.BoxFit.contain,
+      image: image3
+    ),
+  ),
+),
+              ],
+            ),
+          ],
+      ),
+      ];
     })); 
 
     var path = downloadsDirectory.path;
